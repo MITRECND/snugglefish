94b12b41a029afd3ddaffa2c9bcd7f9794485b7d
Shayne Caswell <scaswell@mitre.org>
2013-08-06 13:04:42 -0400

    Implement search function.

--- python/pysnugglefish.cpp
+++ python/pysnugglefish.cpp
@@ -1,48 +1,77 @@
-extern "C" {
-    #include <Python.h>
+#include <Python.h>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include "../src/fileIndexer.cpp"
+#include "../src/nGramBase.cpp"
+#include "../src/nGramIndex.cpp"
+#include "../src/nGramSearch.cpp"
+
+using namespace std;
+
+static PyObject *SnuggleError;
+
+static PyObject * search_wrapper(PyObject * self, PyObject * args)
+{
+    char * indexFileName;
+    char * searchString;
+    int ngramSize;
     
-    static PyObject * search_wrapper(PyObject * self, PyObject * args)
-    {
-        char * indexFileName;
-        char * searchString;
-        int ngramSize;
-        
-        // parse arguments
-        if (!PyArg_ParseTuple(args, "ssi", &indexFileName, &searchString, &ngramSize)) {
-            return NULL;
-        }
-        
-        // run the actual search function
-        //search(indexFileName, searchString, ngramSize);
-        
-        return Py_BuildValue("i", 42);
+    // parse arguments
+    if (!PyArg_ParseTuple(args, "ssi", &indexFileName, &searchString, &ngramSize)) {
+        return NULL;
     }
     
-    static PyObject * index_wrapper(PyObject * self, PyObject * args)
-    {
-        char * indexFileName;
-        char * fileNames;
-        int ngramSize;
-        
-        // parse arguments
-        if (!PyArg_ParseTuple(args, "ssi", &indexFileName, &fileNames, &ngramSize)) {
-            return NULL;
-        }
-        
-        // run the actual indexing function
-        //index(indexFileName, fileNames, ngramSize);
-        
-        return Py_BuildValue("");
+    // run the actual search function
+    if(strlen(searchString) < ngramSize){
+        PyErr_SetString(SnuggleError, "Search string size is smaller than Ngram size, the search string must be greater than or equal to the ngram size");
+        return NULL;
     }
     
-    static PyMethodDef PySnugglefishMethods[] = {
-        { "search", search_wrapper, METH_VARARGS, "Search" },
-        { "index", index_wrapper, METH_VARARGS, "Index" },
-        { NULL, NULL, 0, NULL }
-    };
+    nGramSearch searcher(ngramSize, indexFileName);
+    vector <string> results = searcher.searchNGrams(searcher.stringToNGrams(searchString));
     
-    DL_EXPORT(void) initpysnugglefish(void)
-    {
-        Py_InitModule("pysnugglefish", PySnugglefishMethods);
+    for(uint32_t i = 0; i < results.size(); i++){
+        std::cout << results[i] << std::endl;
+        //printf("%s\n", results[i]);
     }
+    //search(indexFileName, searchString, ngramSize);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject * index_wrapper(PyObject * self, PyObject * args)
+{
+    char * indexFileName;
+    char * fileNames;
+    int ngramSize;
+    
+    // parse arguments
+    if (!PyArg_ParseTuple(args, "ssi", &indexFileName, &fileNames, &ngramSize)) {
+        return NULL;
+    }
+    
+    // run the actual indexing function
+    //index(indexFileName, fileNames, ngramSize);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyMethodDef PySnugglefishMethods[] = {
+    { "search", search_wrapper, METH_VARARGS, "Search an index for an input." },
+    { "index", index_wrapper, METH_VARARGS, "Index a set of input files." },
+    { NULL, NULL, 0, NULL }
+};
+
+PyMODINIT_FUNC initpysnugglefish(void)
+{
+    PyObject *m;
+    
+    m = Py_InitModule("pysnugglefish", PySnugglefishMethods);
+    if (m == NULL)
+        return;
+    
+    SnuggleError = PyErr_NewException("pysnugglefish.error", NULL, NULL);
+    Py_INCREF(SnuggleError);
+    PyModule_AddObject(m, "error", SnuggleError);
 }
--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ LIBRARY_DIRS  = ['/usr/lib', '/usr/local/lib']
 
 # the c++ extension module
 extension_mod = Extension("pysnugglefish",
-                          sources=["pysnugglefish.c"],
+                          sources=["pysnugglefish.cpp"],
                           include_dirs = INCLUDE_DIRS,
                           library_dirs = LIBRARY_DIRS
                           )
