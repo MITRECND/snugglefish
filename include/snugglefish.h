fceb20b20b226688946a66a1bdc16e14be3c764a
Murad Khan <mkhan@mitre.org>
2014-02-19 15:07:05 -0500

    Copyright Bump

--- include/snugglefish.h
+++ include/snugglefish.h
@@ -1,7 +1,7 @@
 /*
  Approved for Public Release; Distribution Unlimited: 13-1937
  
- Copyright (c) 2013 The MITRE Corporation. All rights reserved.
+ Copyright (c) 2014 The MITRE Corporation. All rights reserved.
  
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions
